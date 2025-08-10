2d05dbdb6310fbc515889a71a26d1e7e20e02ba2
Nadzhda vasileva <vasilyeva85@gmail.com>
2025-08-10 14:44:17 +0300

    Изменина основная логика добавлен класс коробки

--- src/main/java/ru/stepup/demo/MainApp.java
+++ src/main/java/ru/stepup/demo/MainApp.java
@@ -5,7 +5,10 @@ public class MainApp {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
-        System.out.println("Hello world!");
+        System.out.println("Hello wor555ld!");
+        System.out.println("1111155gvv!");
+        System.out.println("22");
+        System.out.println("44444");
 
     }
 
---
+++ src/main/java/ru/stepup/demo/box.java
@@ -0,0 +1,23 @@
+package ru.stepup.demo;
+
+public class box {
+
+    private int size;
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public box() {
+    }
+
+    public box(int size) {
+        this.size = size;
+    }
+
+}
+
